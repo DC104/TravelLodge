be8c3edb25d5f7f0d23542bedee307b4adbd8fad
Debosmita Chakraborty <113470640+DC104@users.noreply.github.com>
2024-12-28 15:04:26 +0530

    Update PlacesPage.jsx

--- airbnb-clone/src/pages/PlacesPage.jsx
+++ airbnb-clone/src/pages/PlacesPage.jsx
@@ -33,7 +33,7 @@ export default function PlacesPage() {
             <Link to={`/account/places/${place._id}`} key={place._id} className="flex cursor-pointer bg-gray-200 p-4 rounded-2xl mt-2">
               <div className="w-32 h-32 bg-gray-300 flex-shrink-0">
                 {place.photos.length > 0 && (
-                  <img src={ 'http://localhost:5000/'+ place.photos[0]} alt={place.title} className="w-full h-full object-cover" />
+                  <img src={ 'https://travellodge-3q9e.onrender.com/'+ place.photos[0]} alt={place.title} className="w-full h-full object-cover" />
                 )}
               </div>
               <div className="ml-4 flex-grow">
